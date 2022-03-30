3b4c12b009876445fabc83bee3fc3dabfb91e84e
Travis Racisz <travisracisz@Traviss-MacBook-Pro.local>
2022-03-30 09:06:35 +0900

    made signinwithfacebook a react component

--- pages/auth/signinwithfacebook.js
+++ pages/auth/signinwithfacebook.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { useEffect } from 'react'
 
 
-export default function signInWithFacebook(){
+export default function SignInWithFacebook(){
     const {data: session, status} = useSession()
     console.log(session)
     console.log(status)
