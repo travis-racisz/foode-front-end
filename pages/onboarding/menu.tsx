cc875cfc2dfcb99cd97bba099c3aae03331b3356
Travis Racisz <travisracisz@Traviss-MacBook-Pro.local>
2022-04-25 13:48:52 +0900

    removed Formik from onboarding menu

--- pages/onboarding/menu.tsx
+++ pages/onboarding/menu.tsx
@@ -1,16 +1,16 @@
-import { Field, Formik } from "formik";
+
 
 export default function Menu() {
 	return (
 		<div>
 			<form>
-				<Formik initialValues={{}} onSubmit={ () => console.log("submit")}>
+
 					<h2>Lets add a menu</h2>
-					<Field type="text" placeholder="ex.) Lunch"></Field>
+					<input type="text" placeholder="ex.) Lunch"></input>
 					<h3>What hours is this menu available?</h3>
 					<label>Opening hour</label>
-					<Field type="date" placeholder="ex.) 12:00"></Field>
-				</Formik>
+					<input type="date" placeholder="ex.) 12:00"></input>
+				
 			</form>
 		</div>
 	);
