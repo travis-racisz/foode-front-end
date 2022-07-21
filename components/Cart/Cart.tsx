69eb8b2ad1654a277d99c0bf35a4ff2d1f97cede
Travis Racisz <travisracisz@Traviss-MacBook-Pro.local>
2022-07-21 11:40:46 +0900

    removed optional chaining

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -41,8 +41,8 @@ export default function Cart() {
 			(previous: number, current: Record<string, number>) => previous + current.price,
 			0
 		);
-		context?.setTotal(reducedCart + optionsTotal);
-	}, [context?.cart, optionsTotal, context]);
+		context.setTotal(reducedCart + optionsTotal);
+	}, [context.cart, optionsTotal, context]);
 	return (
 		<div className="cart-sticky">
 			<div onClick={() => setShowCart((prev) => !prev)}>
@@ -50,7 +50,7 @@ export default function Cart() {
 			</div>
 			
 				<div className={showCart ? "show-modal" : "no-display"}>
-					{context?.cart.map((item: Record<string, any>, index: number) => {
+					{context.cart.map((item: Record<string, any>, index: number) => {
 						return (
 							<div
 								className="cart-item"
@@ -87,10 +87,10 @@ export default function Cart() {
 							</div>
 						);
 					})}
-					<h1>Total: ${(context?.total / 100 ).toFixed(2)} </h1>
+					<h1>Total: ${(context.total / 100 ).toFixed(2)} </h1>
 					<button className="cart-checkout-button" onClick={() =>{  
 						if(context.cart.length > 0){
-							context?.addOrder(context.cart, context.total, context.restaurant.id)
+							context.addOrder(context.cart, context.total, context.restaurant.id)
 						} else { 
 							alert("Cart is empty")
 						}
